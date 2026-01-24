c88ac2080ce5087f6373f3194ed815c8d8d14dd5
patelmridul <mridulpatel2000@gamil.com>
2026-01-24 18:09:05 +0530

    update the file

--- 01_Basic/Helloworld.py
+++ 01_Basic/Helloworld.py
@@ -3,3 +3,13 @@ print(2+5) # Addition
 print(2-1) # subtration
 print(2*2) # multipication
 print(4/2) # division   
+print(4**4) # expontial
+
+# Chacking the data types
+print(type(10))
+print(type(2.5))
+print(type(3j+5))
+print(type("Mridul"))
+print(type([1,2,3]))
+print(type({'name':'mridul'}))
+print(type({9.8,3.8,9.8}))
